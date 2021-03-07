763f6ede6a69ea71c1b165ea09197bb71881e66f
theendofunity <dwd96@yandex.ru>
2021-03-07 19:57:05 +0300

    Added JSON parsing

---
+++ HarvardArtMuseumViewer/GalleryData.swift
@@ -0,0 +1,49 @@
+//
+//  GalleryData.swift
+//  HarvardArtMuseumViewer
+//
+//  Created by Admin on 07.03.2021.
+//
+
+import Foundation
+
+struct GalleryData: Decodable {
+    let info: Info
+    let records: [Record]
+}
+
+struct Info: Decodable {
+    let page: Int
+    let pages: Int
+    let totalRecords: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case page
+        case pages
+        case totalRecords = "totalrecords"
+    }
+}
+
+struct Record: Decodable {
+    let classification: String
+    let title: String
+    let objectNumber: String
+    let labelText: String?
+    let imageUrl: String
+    let dateBegin: Int
+    let people: [People]
+    
+    enum CodingKeys: String, CodingKey {
+        case classification
+        case title
+        case objectNumber = "objectnumber"
+        case labelText = "labeltext"
+        case imageUrl = "primaryimageurl"
+        case dateBegin = "datebegin"
+        case people
+    }
+}
+
+struct People : Decodable {
+    let name: String
+}
--- HarvardArtMuseumViewer/NetworkExhibitionManager.swift
+++ HarvardArtMuseumViewer/NetworkExhibitionManager.swift
@@ -9,19 +9,28 @@ import Foundation
 
 class NetworkExhibitionManager {
     func requestData() {
-        let urlString = "https://api.harvardartmuseums.org/gallery?floor=2&apikey=\(apiKey)"
+        let urlString = "https://api.harvardartmuseums.org/object?gallery=2220&apikey=\(apiKey)"
         guard let url = URL(string: urlString) else {
             print("Incorrect URL \(urlString)")
             return
         }
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { data, response, error in
-            if let data = data
-            {
-                let dataString = String(data: data, encoding: .utf8)
-                print(dataString!)
+            if let data = data {
+                self.parseJSON(withData: data)
             }
         }
         task.resume()
+    }
+    
+    func parseJSON(withData data: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let galleryData = try decoder.decode(GalleryData.self, from: data)
+            print(galleryData.info.totalRecords)
+        } catch let error as NSError {
+            print("Error!")
+            print(error.localizedDescription)
+        }
     }
 }
